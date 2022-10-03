09fb62b9b3bec5d050ee0b7a8aba6bd8699dad66
Uran Hajrizaj <uranhajrizaj@outlook.com>
2022-10-03 17:21:22 +0200

    first commit

---
+++ App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args){
+        Cat cat1= new Cat("Gardif",1,true);
+        Cat cat2= new Cat("Oliver",2,false);
+        cat1.DisplayInfo();
+        cat2.DisplayInfo();
+
+    }
+}
+
+
---
+++ Cat.java
@@ -0,0 +1,21 @@
+public class Cat {
+    public String name;
+    public Number age;
+    public Boolean likesCuddles;
+    public String cuddles;
+    public Cat(String name, Number age,Boolean likesCuddles){
+        this.name=name;
+        this.age=age;
+        this.likesCuddles=likesCuddles;
+    }
+    void DisplayInfo(){
+        if(likesCuddles){
+           this.cuddles="likes cuddles";
+        } 
+       else{
+           this.cuddles="doesn't likes cuddles";
+        } 
+        System.out.printf("This is %s, who is %s years old. %s %s %s.\n",this.name,this.age,this.name,this.cuddles);
+    }
+
+}
